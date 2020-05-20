db7ba1f17e021ed21a63d6438c2dd35c0ffb3716
Thomas Clark <anirath@pm.me>
2020-05-20 06:31:18 -0800

    Finished up with argument handling, began work on dealing with options, and started on the actual output.

--- src/diskls.fish
+++ src/diskls.fish
@@ -2,7 +2,7 @@
 # title        : List Disks Fish Commands
 # author       : anirath
 # created      : 2020.05.15
-# modified     : 2020.05.15
+# modified     : 2020.05.20
 # file         : `./diskls.fish`
 # usage        : COMMAND [-OPTIONS] [-o FILENAME]
 # dependencies : `lsblk`, `fish`
@@ -11,82 +11,112 @@
 # Error Handling for List Disks Command
 # =====================================
 function diskls_err -d "Handles errors for the 'diskls' command."
+    # Collect the current error into a variable, and set all of the error messages.
     set -l err_now $argv[1]
-    set -l err_msg_3 "An invalid argument was provided with 'diskls'. Try again with a valid option, or none for defaults."
-    set -l err_msg_4 "An error occurred while creating tmp files. Please try again, or contact the developer if issue persists."
-    set -l err_msg_5 "Too many arguments were provided with `diskls`. Use `diskls -h` or `diskls --help` to view help."
+    set -l err_msg_3 "Invalid arguments! The command was executed with invalid arguments. Run with '-h' to view a help page."
+    set -l err_msg_4 "An error occurred writing temporary files. Please try again, or contact the developer if issue persists."
+    set -l err_msg_5 "Unable to write file to disk. Please double check your output target, and try again."
+    # Display the message for the current error.
     set -l err_msg "err_msg_$err_now"; set_color -o red; printf "Error!!! "; set_color brred; printf "[Code:"$err_now"] ";
     set_color red; printf ""$$err_msg"\n"; set_color normal;
-    if test "$err_now" -eq 3; or test "$err_now" -eq 5
-        set_color -d 999999 grey; printf "\nYour Command Input: "$user_ran"\n"; set_color normal
+    if test "$err_now" -eq 3
+        # If the error is a code 3 show the user their input.
+        set_color -d 999999 grey; printf "Your Command Input: "$user_ran"\n"; set_color normal
     end
+    # Return the function with the corresponding error code.
     return "$err_now"
 end
-# ===========================
-# Autoloaded Command Function
-# ===========================
-function diskls -d "Autoloaded by `fish` to add the 'diskls' command to the shell."
-    # ------------------------
-    # Handle Commandline Input
-    # ------------------------
-    # Initialize command execution by storing the commandline input for later use.
-    set -lx user_ran 'diskls'
-    if test -n "$argv[1]"
-        set user_ran "diskls $argv"
-    else
-        # If no arguments were provided set the local variable `argv` with 'null' and export it.
-        set -lx argv 'null'
-    end
+# ================================
+# Help Page for List Disks Command
+# ================================
+function diskls_help -d "Prints a help message for the 'diskls' command."
+    echo "Help me!"; and return 0
+end
+# ==========================================
+# Autoloaded Function for List Disks Command
+# ==========================================
+function diskls -d "Autoloaded by 'fish' to create the disk list command 'diskls'."
     # --------------------------------
     # Parse Arguments & Toggle Options
     # --------------------------------
-    # If the `$argv` variable wasn't set to 'null' during intializing then parse arguments.
-    if not test "$argv" = 'null'
-        # Create an `$option` variable with value 'null'.
-        set -l option 'null'
-        # Parse arguments for the help option, toggle the option if enabled, and handle any errors.
-        argparse -n=diskls -x h,a,d,f,k,p,u 'h/help' 'a/all' 'd/disk' 'f/filesystem' 'k/kernel' 'p/partition' 'u/unique' 'o=' -- $argv
-        # Check for any invalid arguments after parsing for options toggles, and throw the corresponding error if needed.
-        if test "$status" -eq 1
-            diskls_err 5
-        else if test (count "$argv") -gt 0
-            diskls_err 3
+    # Export variables for the commandline input, and the available options.
+    set -lx user_ran "diskls $argv"; set -lx option ''; set -lx output_target ''; set -lx diskls_columns '';
+    set -lx options 'h/help' 'a/all' 'd/disk' 'f/filesystem' 'k/kernel' 'u/unique' 'o='
+    # Check for any arguments.
+    if set -q argv[1]
+        # Parse the arguments to toggle option flags.
+        argparse -x h,a,d,f,k,u -x h,o $options -- $argv
+        if test "$status" -gt 0; or set -q argv[1]; diskls_err 3; return $status; end
+        # Check the `argparse` output and set the toggled option.
+        if set -q _flag_h
+            #If the help option was enabled just run diskls_help() and return 0.
+            diskls_help; and return 0
+        else if set -q _flag_a
+            set option 'all'
+        else if set -q _flag_d
+            set option 'disk'
+        else if set -q _flag_f
+            set option 'filesystem'
+        else if set -q _flag_k
+            set option 'kernel'
+        else if set -q _flag_u
+            set option 'unique'
+        else
+            set option 'default'
         end
-
-        if set -q _flag_h; or set -q _flag_help
-        else if not set -q _flag_a; or not set -q _flag_all;
-            set -l option 'all'
-        else if not set -q _flag_d; or not set -q _flag_disk
-            set -l option 'disk'
-        else if not set -q _flag_f; or not set -q _flag_filesystem
-            set -l option 'filesystem'
-        else if not set -q _flag_k; or not set -q _flag_kernel
-            set -l option 'kernel'
-        else if not set -q _flag_d; or not set -q _flag_disk
-            set -l option 'disk'
-        else if not set -q _flag_d; or not set -q _flag_disk
-            set -l option 'disk'
-        end
-        # Parse the remaining arguments for the output option.
-        argparse -n=diskls_output  -- $argv
+        # Set the $output_target if provided with the option.
         if set -q _flag_o
-            # If the output option was enabled export a variable with the filepath.
-            set -lx diskls_output "$_flag_o"
-            if test -d "$diskls_output"
-                # If the provided filepath points to a directory then handle the error.
-                diskls_err 1
-            else if test -e "$diskls_output"
-                # If the file already exists then handle the error.
-                diskls_err 1
-            else
-                # Create the output file now, and handle an error if it occurs.
-                touch "$diskls_output"; or diskls_err 1
-            end
+            set output_target "$_flag_o"
+        else
+            set output_target 'null'
         end
+    else
+        # If there are no arguments then set the option to default, and a null target.
+        set option 'default'; set output_target 'null'
     end
-    # ---------------------
-    # Output Temporary Data
-    # ---------------------
-    # Create the tmp file and assign a variable to point to it, or handle the error and return the status.
-    set -l tmp_file (mktemp /tmp/dls.XXXXXXXXX) or dls_err 4; and return "$status"
+    # ----------------------
+    # Validate Output Target
+    # ----------------------
+    if test "$output_target" != 'null'
+        # If the output to file option was enabled then verify the ability to write.
+        if test -f "$output_target"; and test -w "$output_target"
+            # Prompt the user to confirm if they want to overwrite an existing file at that location.
+            printf "Target: $output_target \n"
+            printf "The output target you provided is already an existing file. Would you like to overwrite it? [Y/n]\n"
+        else if test -f "$output_target"
+            # Let the user know the target is not writable, and return from the command.
+            printf "Unfortunately the output target you provided points to an existing file that isn't writable. Please try again\n"
+            printf "with a valid target.\n"
+            return
+        end
+        # Create the output target file or process an error.
+        touch "$output_target"
+            or diskls_err 5 and return $status
+    end
+    # --------------
+    # Handle Options
+    # --------------
+    switch "$option"
+        case 'default'
+            set diskls_columns 'NAME,TYPE,PTTYPE,MOUNTPOINT,PATH,PARTLABEL,FSTYPE,FSUSE%,SIZE,FSUSED'
+        case 'disk'
+            set diskls_columns 'NAME,TYPE,MOUNTPOINT,PATH,MAJ:MIN,MODEL,SUBSYSTEMS,PHY-SEC,VENDOR,HOTPLUG'
+        case 'filesystem'
+            set diskls_columns 'NAME,TYPE,LABEL,MOUNTPOINT,PATH,FSTYPE,FSUSE%,FSSIZE,FSAVAIL,FSUSED'
+        case 'kernel'
+            set diskls_columns 'NAME,KNAME,TYPE,MOUNTPOINT,PATH,SUBSYSTEMS,STATE,RQ-SIZE'
+        case 'unique'
+            set diskls_columns 'NAME,PATH,MODEL,SERIAL,UUID,WWN'
+        case '*'
+            return 1
+    end
+    # ------------
+    # Print Output
+    # ------------
+    if test "$output_target" != 'null'
+        # If the output target is set then write to it.
+        lsblk -e7,252 -o "$diskls_columns" >> "$output_target"
+    end
+    # Print the output to the terminal.
+    lsblk -e7,252 -o "$diskls_columns"
 end
